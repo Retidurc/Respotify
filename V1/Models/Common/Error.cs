c3c1dfe351bb60e0823cd828e17b6dde34a3c951
Retidurc Silvernight <retidurc@gmail.com>
2019-10-24 17:22:45 +0200

    Create base object for api responses

---
+++ V1/Models/Common/Error.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remotify.V1.Models.Common
+{
+    public class Error
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ V1/Models/Common/Responses/BaseResponse.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remotify.V1.Models.Common.Responses
+{
+    class BaseResponse
+    {
+        Error error { get; set; }
+    }
+}
